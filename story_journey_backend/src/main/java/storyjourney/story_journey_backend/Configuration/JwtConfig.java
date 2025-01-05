98780001975d879bd83a2607ccc53ff706793b07
iremtektas <tektasirem13@gmail.com>
2025-01-05 17:50:52 +0300

    jwt ayarlamaları yeniden düzenlendi

--- story_journey_backend/src/main/java/storyjourney/story_journey_backend/Configuration/JwtConfig.java
+++ story_journey_backend/src/main/java/storyjourney/story_journey_backend/Configuration/JwtConfig.java
@@ -1,19 +1,24 @@
 package storyjourney.story_journey_backend.Configuration;
 
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import storyjourney.story_journey_backend.Service.JwtService;
+import storyjourney.story_journey_backend.Utils.SecretKeyLoader;
+
 
 @Configuration
 public class JwtConfig {
-    
-    @Value("${jwt.secretKey}")
-    private String secretKey;
-    
+
+    private final SecretKeyLoader secretKeyLoader;
+
+    public JwtConfig(SecretKeyLoader secretKeyLoader) {
+        this.secretKeyLoader = secretKeyLoader;
+    }
+
     @Bean
     public JwtService jwtService() {
-        return new JwtService(secretKey);
+        return new JwtService(secretKeyLoader.getSecretKey());
     }
 }
