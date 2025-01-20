cf322c90c25c8445262e59d9cb7af01e9ade9dda
iremtektas <tektasirem13@gmail.com>
2025-01-20 23:42:03 +0300

    mobil projesi başlatıldı

---
+++ StoryJourneyMobile/StoryJourneyMobileTests/StoryJourneyMobileTests.swift
@@ -0,0 +1,17 @@
+//
+//  StoryJourneyMobileTests.swift
+//  StoryJourneyMobileTests
+//
+//  Created by iremt on 20.01.2025.
+//
+
+import Testing
+@testable import StoryJourneyMobile
+
+struct StoryJourneyMobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
